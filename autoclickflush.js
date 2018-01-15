b7da1d386e93ed41f84d678ea7fc5b0a5cf45df7
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-01-15 14:40:23 +0800

    新增代码

---
+++ autoclickflush.js
@@ -0,0 +1,16 @@
+﻿var timecount=0;
+var nums=[
+"18XXXXXXXXX",
+"18XXXXXXXXX"
+]
+function autoClick(){
+	var index = Math.floor((Math.random()*nums.length));
+	$("#busiNumSmall").val(nums[index]);
+        $("#diagnoseSmall").click(); 
+        timecount+=1;
+       if(timecount=== 7){
+       clearInterval(interval);
+       } 
+}
+
+var interval=setInterval(autoClick,10000);
